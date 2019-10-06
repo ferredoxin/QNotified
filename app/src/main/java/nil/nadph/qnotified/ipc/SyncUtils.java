0dbb177aba4cbf2461bac80d4653757029dc41b2
cinit <xenonhydride@gmail.com>
2019-10-06 22:20:06 +0800

    failed

--- app/src/main/java/nil/nadph/qnotified/ipc/SyncUtils.java
+++ app/src/main/java/nil/nadph/qnotified/ipc/SyncUtils.java
@@ -1,73 +1,82 @@
 package nil.nadph.qnotified.ipc;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.LocalSocket;
+import android.net.LocalSocketAddress;
+import libcore.io.Libcore;
+
 import java.io.*;
+import java.util.UUID;
 
 import static nil.nadph.qnotified.util.Utils.*;
-import android.content.*;
-import java.util.*;
-import android.net.*;
 
+@SuppressLint("PrivateApi")
 public class SyncUtils {
-	
-	public static void startServerSocket(){
-		String name=getSocketUuid();
-		try {
-			LocalServerSocket server=new LocalServerSocket(name);
-			server.accept().
-		} catch (IOException e) {
-			log(e);
-		}
-	}
 
-	public static boolean isMainProcess() {
-		try {
-			FileInputStream fin = new FileInputStream("/proc/" + android.os.Process.myPid() + "/cmdline");
-			byte[] b = new byte[64];
-			int len = fin.read(b, 0, b.length);
-			fin.close();
-			String procName = new String(b, 0, len).trim();
-			return !procName.contains(":");
-		} catch (IOException e) {
-			log(e);
-			//should not happen
-			return true;
-		}
-	}
+    public static void startServerSocket() {
+        getSocketUuid();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String name = getSocketUuid();
+                try {
+                    LocalSocketAddress addr = new LocalSocketAddress(name, LocalSocketAddress.Namespace.ABSTRACT);
+                    Object impl = new_instance(Context.class.getClassLoader().loadClass("android.net.LocalSocketImpl"));
+                    invoke_virtual(impl, "create", LocalSocket.SOCKET_DGRAM, int.class);
+                    invoke_virtual(impl, "bind", addr, LocalSocketAddress.class);
+                    FileDescriptor fd= (FileDescriptor) iget_object_or_null(impl,"fd");
 
-	public static synchronized String getSocketUuid() {
-		Context ctx=getApplication();
-		File f=new File(ctx.getFilesDir(), "qnotified_uuid");
-		try {
-			if (f.exists()) {
-				FileInputStream fin=new FileInputStream(f);
-				byte[]buf=new byte[20];
-				int l=fin.read(buf);
-				fin.close();
-				String str=new String(buf, 0, l)
-					.replace("\r", "").replace("\n", "").replace(" ", "").replace("\t", "");
-				if (str.length() > 4)return str;
-			}
-			String uuid=UUID.randomUUID().toString().replace("\r", "").replace("\n", "").replace(" ", "").replace("\t", "");;
-			if (!f.exists())f.createNewFile();
-			FileOutputStream fout=new FileOutputStream(f);
-			fout.write(uuid.getBytes());
-			fout.flush();
-			fout.close();
-			return uuid;
-		} catch (IOException e) {
-			throw new RuntimeException("Unable to allocate uuid");
-		}
-	}
+                } catch (Exception e) {
+                    log(e);
+                }
 
-	public static int getUid(){
-		FileInputStream fin = new FileInputStream("/proc/" + android.os.Process.myPid() + "/status");
-		Scanner scan=new Scanner(fin);
-		String line;
-		while((line=scan.nextLine())!=null){
-			if(line.startsWith("Uid")){
-				line=line.replace(
-			}
-		}
-	}
-	
+            }
+        }).start();
+    }
+
+    public static boolean isMainProcess() {
+        try {
+            FileInputStream fin = new FileInputStream("/proc/" + android.os.Process.myPid() + "/cmdline");
+            byte[] b = new byte[64];
+            int len = fin.read(b, 0, b.length);
+            fin.close();
+            String procName = new String(b, 0, len).trim();
+            return !procName.contains(":");
+        } catch (IOException e) {
+            log(e);
+            //should not happen
+            return true;
+        }
+    }
+
+    public static synchronized String getSocketUuid() {
+        Context ctx = getApplication();
+        File f = new File(ctx.getFilesDir(), "qnotified_uuid");
+        try {
+            if (f.exists()) {
+                FileInputStream fin = new FileInputStream(f);
+                byte[] buf = new byte[20];
+                int l = fin.read(buf);
+                fin.close();
+                String str = new String(buf, 0, l)
+                        .replace("\r", "").replace("\n", "").replace(" ", "").replace("\t", "");
+                if (str.length() > 4) return str;
+            }
+            String uuid = UUID.randomUUID().toString().replace("\r", "").replace("\n", "").replace(" ", "").replace("\t", "");
+            if (!f.exists()) f.createNewFile();
+            FileOutputStream fout = new FileOutputStream(f);
+            fout.write(uuid.getBytes());
+            fout.flush();
+            fout.close();
+            return uuid;
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to allocate uuid");
+        }
+    }
+
+    public static int getUid() {
+        return Libcore.os.getuid();
+    }
+
 }
