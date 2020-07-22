be593ac1aa953c5986fce6ad961d09f9566d6805
Null Idle <xenonhydride@gmail.com>
2020-07-22 14:30:30 +0800

    sync

--- app/src/main/java/nil/nadph/qnotified/ExfriendManager.java
+++ app/src/main/java/nil/nadph/qnotified/ExfriendManager.java
@@ -1,19 +1,19 @@
 /* QNotified - An Xposed module for QQ/TIM
- * Copyright (C) 2019-2020 cinit@github.com
+ * Copyright (C) 2019-2020 xenonhydride@gmail.com
  * https://github.com/cinit/QNotified
  *
  * This software is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.  If not, see
+ * You should have received a copy of the GNU General Public License
+ * along with this software.  If not, see
  * <https://www.gnu.org/licenses/>.
  */
 package nil.nadph.qnotified;
