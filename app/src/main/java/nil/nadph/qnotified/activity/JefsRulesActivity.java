12ea44d9fb746437835f206fa97649f39c3254d8
Null Idle <xenonhydride@gmail.com>
2020-07-30 02:32:49 +0800

    Add description

--- app/src/main/java/nil/nadph/qnotified/activity/JefsRulesActivity.java
+++ app/src/main/java/nil/nadph/qnotified/activity/JefsRulesActivity.java
@@ -20,6 +20,7 @@ package nil.nadph.qnotified.activity;
 
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -34,6 +35,7 @@ import nil.nadph.qnotified.ui.HighContrastBorder;
 import nil.nadph.qnotified.ui.ResUtils;
 import nil.nadph.qnotified.ui.ViewBuilder;
 import nil.nadph.qnotified.util.UiThread;
+import nil.nadph.qnotified.util.Utils;
 
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -41,6 +43,7 @@ import java.util.ArrayList;
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 import static nil.nadph.qnotified.util.Utils.dip2px;
+import static nil.nadph.qnotified.util.Utils.log;
 
 @SuppressLint("Registered")
 public class JefsRulesActivity extends IphoneTitleBarActivityCompat implements View.OnClickListener {
@@ -81,6 +84,20 @@ public class JefsRulesActivity extends IphoneTitleBarActivityCompat implements V
             layoutDisplay = new LinearLayout(this);
             layoutDisplay.setOrientation(LinearLayout.VERTICAL);
             layoutDisplay.setId(R.id.jefsRulesDisplayLayout);
+            {
+                String appLabel = "QQ/TIM";
+                try {
+                    PackageInfo pi = Utils.getHostInfo(this);
+                    appLabel = pi.applicationInfo.loadLabel(this.getPackageManager()).toString();
+                } catch (Throwable e) {
+                    log(e);
+                }
+                TextView _tmp_1 = new TextView(this);
+                _tmp_1.setTextColor(ResUtils.skin_gray3);
+                _tmp_1.setText("本功能用于去除恼人的 \"即将离开" + appLabel + " 前往其他应用\" 对话框, " +
+                        "也可用于限制或禁止" + appLabel + "跳转到特定第三方APP或启动某特定活动(自身的也行)");
+                layoutDisplay.addView(_tmp_1, ViewBuilder.newLinearLayoutParams(MATCH_PARENT, WRAP_CONTENT, __5));
+            }
             rulesTv = new TextView(this);
             rulesTv.setTextSize(14);
             rulesTv.setPadding(__5, __5, __5, __5);
