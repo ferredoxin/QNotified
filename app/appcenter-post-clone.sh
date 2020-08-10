5e4a1f3ae3f2d63804156f056731675bdb7aa83f
singleNeuron <liziyuan123456@yeah.net>
2020-08-10 14:15:09 +0200

    [skip ci] update app center script

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-commit=$(git show -s --format="%s")
+commit=$(git show -s --format="%s %b")
 echo "commit: $commit"
 if [[ $commit == *"[skip ci]"* ]]; then
     curl -i -X PATCH -H "X-API-Token:$APPCENTER_API_TOKEN" -H "Content-Type: application/json" -d "{\"status\":\"cancelling\"}" https://appcenter.ms/api/v0.1/apps/QNotifiedDev/QNotified/builds/$APPCENTER_BUILD_ID
