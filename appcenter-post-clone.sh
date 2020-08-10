882b92955bf59f3315711c3e3fd59e2a98d16fbe
singleNeuron <liziyuan123456@yeah.net>
2020-08-10 13:18:15 +0200

    [skip ci] try to add skip ci for appcenter

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "commit: $commit"
+if [[ $commit == *"[skip ci]"* ]]; then
+    curl -i -X PATCH -H "X-API-Token:$APPCENTER_API_TOKEN" -H "Content-Type: application/json" -d "{\"status\":\"cancelling\"}" https://appcenter.ms/api/v0.1/apps/QNotifiedDev/QNotified/builds/$APPCENTER_BUILD_ID
+else
+    echo "Continue building"
+fi
