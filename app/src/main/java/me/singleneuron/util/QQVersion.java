a829ec0d5bdcaf0c20b09c221c94fbefff31be59
MaiDongTM <49202599+Lagrio@users.noreply.github.com>
2020-08-19 21:00:36 +0800

    Add more QQ versions
    Add more QQ versions

--- app/src/main/java/me/singleneuron/util/QQVersion.java
+++ app/src/main/java/me/singleneuron/util/QQVersion.java
@@ -1,12 +1,19 @@
-package me.singleneuron.util;
+ package me.singleneuron.util;
 
 public class QQVersion {
-
+    public static final int QQ_8_0_5 = 1186;
+    public static final int QQ_8_0_7 = 1204;
+    public static final int QQ_8_0_8 = 1218;
+    public static final int QQ_8_1_0 = 1232;  
     public static final int QQ_8_1_3 = 1246;
+    public static final int QQ_8_1_5 = 1258;
+    public static final int QQ_8_1_8 = 1276;
     public static final int QQ_8_2_0 = 1296;
     public static final int QQ_8_2_6 = 1320;
     public static final int QQ_8_2_7 = 1334;
     public static final int QQ_8_2_8 = 1346;
+    public static final int QQ_8_2_9 = 1352;
+    public static final int QQ_8_2_10 = 1354;
     public static final int QQ_8_3_0 = 1362;
     public static final int QQ_8_3_3 = 1376;
     public static final int QQ_8_3_5 = 1392;
