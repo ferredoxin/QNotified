a24d8c44a33b4c4bf1ae7fca2103ea212a24b2b8
singleNeuron <cryolitia@gmail.com>
2020-08-20 18:21:57 +0200

    add more kotlin wheel
    Signed-off-by: singleNeuron <Cryolitia@gmail.com>

--- app/src/main/java/me/singleneuron/util/KotlinUtils.kt
+++ app/src/main/java/me/singleneuron/util/KotlinUtils.kt
@@ -5,8 +5,15 @@ import android.view.View
 import android.view.ViewGroup
 import me.singleneuron.base.BaseDelayableConditionalHookAdapter
 import nil.nadph.qnotified.ui.ViewBuilder.newListItemHookSwitchInit
+import java.io.BufferedReader
+import java.io.File
 
 class KotlinUtils {
+
+    /*
+        use in Java:
+        KotlinUtils.Companion.readFile(yourFile)
+     */
 
     companion object {
         fun ViewGroup.addViewConditionally(view: View, condition: ()->Boolean) {
@@ -18,6 +25,15 @@ class KotlinUtils {
         fun ViewGroup.addViewConditionally(context: Context, title: String, desc:String, hook:BaseDelayableConditionalHookAdapter) {
             addViewConditionally(newListItemHookSwitchInit(context,title,desc,hook),hook.condition)
         }
+
+        fun readFile(file: File) : String {
+            return file.readText()
+        }
+
+        fun readFromBufferedReader(bufferedReader: BufferedReader) : String{
+            return bufferedReader.readText()
+        }
+
     }
 
 }
