fa7f29a16758109228c22afed83b89545a5c98b4
singleNeuron <cryolitia@gmail.com>
2020-08-22 16:43:13 +0200

    let force use system xxx support 8.4.1
    Signed-off-by: singleNeuron <Cryolitia@gmail.com>

--- app/src/main/java/me/singleneuron/hook/ForceSystemAlbum.kt
+++ app/src/main/java/me/singleneuron/hook/ForceSystemAlbum.kt
@@ -1,31 +1,16 @@
 package me.singleneuron.hook
 
-import android.app.Activity
 import android.content.Intent
 import de.robv.android.xposed.XposedBridge
 import me.singleneuron.activity.ChooseAlbumAgentActivity
 import me.singleneuron.base.BaseDelayableConditionalHookAdapter
 import me.singleneuron.util.QQVersion
-import nil.nadph.qnotified.SyncUtils
 import nil.nadph.qnotified.util.Utils
 
 object ForceSystemAlbum : BaseDelayableConditionalHookAdapter("forceSystemFile") {
 
     override fun doInit(): Boolean {
-        //val albumClass = Class.forName("com.tencent.mobileqq.activity.photo.album.NewList")
-
-        /* for debug only
-        val hook = object : XposedMethodHookAdapter() {
-            override fun beforeMethod(param: MethodHookParam?) {
-                val intent : Intent = param!!.args[0] as Intent
-                Utils.logd("singleNeuron")
-                Utils.logd(intent.toString())
-                Utils.log(Throwable())
-            }
-        }
-        XposedBridge.hookAllMethods(Activity::class.java,"startActivity", hook)
-        XposedBridge.hookAllMethods(Activity::class.java,"startActivityForResult", hook)*/
-
+        //特征字符串:"onAlbumBtnClicked"
         val photoListPanelClass = Class.forName("com.tencent.mobileqq.activity.aio.photo.PhotoListPanel")
         XposedBridge.hookAllMethods(photoListPanelClass,"e",object : XposedMethodHookAdapter() {
             override fun beforeMethod(param: MethodHookParam?) {
@@ -38,6 +23,6 @@ object ForceSystemAlbum : BaseDelayableConditionalHookAdapter("forceSystemFile")
     }
 
     override val condition: () -> Boolean
-        get() = {Utils.getHostVersionCode()==QQVersion.QQ_8_3_9}
+        get() = {Utils.getHostVersionCode()==QQVersion.QQ_8_3_9 || Utils.getHostVersionCode()==QQVersion.QQ_8_4_1}
 
 }
--- app/src/main/java/me/singleneuron/hook/ForceSystemCamera.kt
+++ app/src/main/java/me/singleneuron/hook/ForceSystemCamera.kt
@@ -6,27 +6,31 @@ import me.singleneuron.base.BaseDelayableConditionalHookAdapter
 import me.singleneuron.util.QQVersion
 import nil.nadph.qnotified.util.LicenseStatus
 import nil.nadph.qnotified.util.Utils
-import java.lang.Exception
 
 object ForceSystemCamera : BaseDelayableConditionalHookAdapter("forceSystemCamera") {
     override fun doInit(): Boolean {
-        try {
-            val babg = Class.forName("babg")
-            XposedHelpers.findAndHookMethod(babg,"a",object : XC_MethodHook(){
-                override fun afterHookedMethod(param: MethodHookParam?) {
-                    if (LicenseStatus.sDisableCommonHooks) return
-                    if (!isEnabled) return
-                    Utils.logd("singleneuron babq.a():"+(param!!.result as Boolean))
-                    param.result = false
-                }
-            })
-        } catch (e:Exception) {
-            Utils.log(e)
-            return false
-        }
+        //特征字符串："CaptureUtil"
+        val captureUtilClass = Class.forName(getClass())
+        //特征字符串："GT-I9500"
+        XposedHelpers.findAndHookMethod(captureUtilClass,"a",object : XC_MethodHook(){
+            override fun afterHookedMethod(param: MethodHookParam?) {
+                if (LicenseStatus.sDisableCommonHooks) return
+                if (!isEnabled) return
+                Utils.logd("ForceSystemCamera babq.a():"+(param!!.result as Boolean))
+                param.result = false
+            }
+        })
         return true
     }
 
     override val condition: () -> Boolean
-        get() = {Utils.getHostVersionCode()== QQVersion.QQ_8_3_9}
+        get() = {Utils.getHostVersionCode()== QQVersion.QQ_8_3_9 || Utils.getHostVersionCode()==QQVersion.QQ_8_4_1}
+
+    override fun getClass(): String {
+        return when(Utils.getHostVersionCode()) {
+            QQVersion.QQ_8_3_9 -> "babg"
+            QQVersion.QQ_8_4_1 -> "bann"
+            else -> super.getClass()
+        }
+    }
 }
