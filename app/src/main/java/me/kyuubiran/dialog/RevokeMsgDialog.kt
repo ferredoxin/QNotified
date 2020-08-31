501d833b5e4c1f3d6046ffd7801beb2a8a7f3c51
KyuubiRan <KyuubiRan@qq.com>
2020-08-31 21:12:50 +0800

    support viewBinding update RevokeMsgDialog.kt

--- app/src/main/java/me/kyuubiran/dialog/RevokeMsgDialog.kt
+++ app/src/main/java/me/kyuubiran/dialog/RevokeMsgDialog.kt
@@ -5,22 +5,22 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.CheckBox
 import android.widget.CompoundButton
-import android.widget.EditText
-import android.widget.LinearLayout
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import me.kyuubiran.hook.RevokeMsg
 import me.kyuubiran.utils.getDefaultCfg
 import nil.nadph.qnotified.R
+import nil.nadph.qnotified.databinding.KyuubiranRevokeMsgDialogBinding
 
-object RevokeMsgDialog {
+object RevokeMsgDialog{
     private var currentEnable: Boolean? = null
     private var currentMsgEnable: Boolean? = null
     private var currentRevokeTips = ""
     private var currentUnreceivedTips = ""
+    private lateinit var binding: KyuubiranRevokeMsgDialogBinding
 
     fun onShow(ctx: Context) {
+        binding = KyuubiranRevokeMsgDialogBinding.inflate(LayoutInflater.from(ctx))
         val cfg = getDefaultCfg()
         val enable = cfg.getBooleanOrFalse(RevokeMsg.kr_revoke_msg)
         val showMsgEnable = cfg.getBooleanOrFalse(RevokeMsg.kr_revoke_msg_show_msg_text_enabled)
@@ -30,60 +30,49 @@ object RevokeMsgDialog {
         currentEnable = enable
         currentMsgEnable = showMsgEnable
 
-        val mViewGroup = LayoutInflater.from(ctx).inflate(R.layout.kyuubiran_revoke_msg_dialog, null)
-        val mEnable = mViewGroup.findViewById<CheckBox>(R.id.kr_revoke_msg_enabled)
-        val mShowMsgEnable = mViewGroup.findViewById<CheckBox>(R.id.kr_revoke_msg_show_msg_text_enabled)
-        val mPanel = mViewGroup.findViewById<LinearLayout>(R.id.kr_revoke_msg_panel)
-        val mRevokedMsgTips = mViewGroup.findViewById<EditText>(R.id.kr_revoked_msg_tips_text)
-        val mUnreceivedRevokedMsgTips = mViewGroup.findViewById<EditText>(R.id.kr_unreceived_revoked_msg_tips_text)
         val mDialog = MaterialAlertDialogBuilder(ctx, R.style.MaterialDialog)
 
-        mEnable.isChecked = enable
-        mPanel.visibility = if (enable) View.VISIBLE else View.GONE
-        mShowMsgEnable.isChecked = showMsgEnable
-        if (mRevokedMsgTips.text.isEmpty()) mRevokedMsgTips.setText(revokedMsgTips)
-        if (mUnreceivedRevokedMsgTips.text.isEmpty()) mUnreceivedRevokedMsgTips.setText(unreceivedRevokedMsgTips)
+        binding.krRevokeMsgEnabled.isChecked = enable
+        binding.krRevokeMsgPanel.visibility = if (enable) View.VISIBLE else View.GONE
+        binding.krRevokeMsgShowMsgTextEnabled.isChecked = showMsgEnable
+        if (binding.krRevokedMsgTipsText.text.isEmpty()) binding.krRevokedMsgTipsText.setText(revokedMsgTips)
+        if (binding.krUnreceivedRevokedMsgTipsText.text.isEmpty()) binding.krUnreceivedRevokedMsgTipsText.setText(unreceivedRevokedMsgTips)
         if (currentRevokeTips.isEmpty()) currentRevokeTips = revokedMsgTips
         if (currentUnreceivedTips.isEmpty()) currentUnreceivedTips = unreceivedRevokedMsgTips
 
-
-        mEnable.setOnCheckedChangeListener { _: CompoundButton, b: Boolean ->
-            mPanel.visibility = if (b) View.VISIBLE else View.GONE
+        binding.krRevokeMsgEnabled.setOnCheckedChangeListener { _: CompoundButton, b: Boolean ->
+            binding.krRevokeMsgPanel.visibility = if (b) View.VISIBLE else View.GONE
             currentEnable = b
         }
 
-        mShowMsgEnable.setOnCheckedChangeListener { _: CompoundButton, b: Boolean ->
+        binding.krRevokeMsgShowMsgTextEnabled.setOnCheckedChangeListener { _: CompoundButton, b: Boolean ->
             currentMsgEnable = b
         }
 
-        mRevokedMsgTips.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                //empty
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                //empty
-            }
-
+        binding.krRevokedMsgTipsText.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 currentRevokeTips = s.toString()
             }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
         })
-        mUnreceivedRevokedMsgTips.addTextChangedListener(object : TextWatcher {
+        binding.krUnreceivedRevokedMsgTipsText.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 currentUnreceivedTips = s.toString()
             }
 
             override fun afterTextChanged(s: Editable?) {
-                //empty
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                //empty
             }
         })
 
-        mDialog.setView(mViewGroup)
+        mDialog.setView(binding.root)
         mDialog.setPositiveButton("保存") { _, _ -> save() }
         mDialog.setNegativeButton("取消") { _, _ -> }
         mDialog.setCancelable(false)
