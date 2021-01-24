11f62c7f8b8ab1637bac961f6b2db7828c5bda0d
NextAlone <12210746+NextAlone@users.noreply.github.com>
2021-01-24 17:41:57 +0800

    Remove ID Configs in QQConfigTable.kt
    Signed-off-by: NextAlone <12210746+NextAlone@users.noreply.github.com>

--- app/src/main/java/me/nextalone/hook/ForcedSendOriginalPhoto.kt
+++ app/src/main/java/me/nextalone/hook/ForcedSendOriginalPhoto.kt
@@ -5,10 +5,10 @@ import android.widget.CheckBox
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
 import me.kyuubiran.util.getMethods
-import me.singleneuron.qn_kernel.tlb.ConfigTable
 import nil.nadph.qnotified.hook.CommonDelayableHook
 import nil.nadph.qnotified.util.LicenseStatus
 import nil.nadph.qnotified.util.Utils
+import nil.nadph.qnotified.util.Utils.PACKAGE_NAME_QQ
 import java.lang.reflect.Method
 
 object ForcedSendOriginalPhoto : CommonDelayableHook("na_test_forced_original") {
@@ -24,54 +24,25 @@ object ForcedSendOriginalPhoto : CommonDelayableHook("na_test_forced_original") 
                             if (LicenseStatus.sDisableCommonHooks) return
                             if (!isEnabled) return
                             val ctx = param!!.thisObject as View
-                            val sendOriginPhotoCheckbox: CheckBox = ctx.findViewById(ConfigTable.getConfig(ForcedSendOriginalPhoto::class.simpleName))
+                            val id = ctx.resources.getIdentifier("h1y", "id", PACKAGE_NAME_QQ)
+                            val sendOriginPhotoCheckbox: CheckBox = ctx.findViewById(id)
+//                            val sendOriginPhotoCheckbox: CheckBox = ctx.findViewById(ConfigTable.getConfig(ForcedSendOriginalPhoto::class.simpleName))
                             sendOriginPhotoCheckbox.isChecked = true
 //                            Utils.logd("TestForcedOriginal: Return checkbox isChecked" + sendOriginPhotoCheckbox.isChecked)
 //                            Utils.logd("TestForcedOriginal: Set checkbox checked to send original photo")
                         }
                     })
                 }
-            }//com.tencent.mobileqq.activity.photo.album.AbstractPhotoPreviewActivity
-//            for (m: Method in getMethods("com.tencent.mobileqq.activity.photo.album.AbstractPhotoPreviewActivity")) {
-//                val argt = m.parameterTypes
-//                if (m.name == "onCreate" && argt.size == 1) {
-//                    logi("TestForcedOriginal: Found AbstractPhotoPreviewActivity onCreate")
-//                    XposedBridge.hookMethod(m, object : XC_MethodHook() {
-//                        @SuppressLint("ResourceType")
-//                        override fun afterHookedMethod(param: MethodHookParam?) {
-//                            if (LicenseStatus.sDisableCommonHooks) return
-////                            if (!isEnabled) return
-//                            logi("TestForcedOriginal: AbstractPhotoPreviewActivity onCreate")
-//                            val ctx = param!!.thisObject as View
-//                            val sendOriginPhotoCheckbox: CheckBox = ctx.findViewById(ConfigTable.getConfig(ForcedSendOriginalPhoto::class.simpleName))
-////                           val sendOriginPhotoCheckbox: CheckBox = ctx.findViewById(2131375226)
-////                            sendOriginPhotoCheckbox.isChecked = true
-//                            Utils.logd("TestForcedOriginal: Return checkbox isChecked " + sendOriginPhotoCheckbox.isChecked)
-////                            Utils.logd("TestForcedOriginal: Set checkbox checked to send original photo")
-//                        }
-//                    })
+            }
+//            XposedBridge.hookAllConstructors(loadClass("com.tencent.mobileqq.activity.photo.album.NewPhotoPreviewActivity"), object : XC_MethodHook() {
+//                override fun afterHookedMethod(param: MethodHookParam?) {
+//                    if (LicenseStatus.sDisableCommonHooks) return
+//                    if (!isEnabled) return
+//                    val ctx = param!!.thisObject as View
+//                    val sendOriginPhotoCheckbox: CheckBox = ctx.findViewById(ConfigTable.getConfig(ForcedSendOriginalPhoto::class.simpleName))
+//                    sendOriginPhotoCheckbox.isChecked = true
 //                }
-//            }
-//            for (m: Method in getMethods("com.tencent.mobileqq.activity.photo.album.PhotoPreviewLogicDefault")) {
-//                val argt = m.parameterTypes
-//                if (m.name == "initUI" && argt.isEmpty()) {
-//                    logi("TestForcedOriginal: Found PhotoPreviewLogicDefault")
-//                    XposedBridge.hookMethod(m, object : XC_MethodHook() {
-//                        @SuppressLint("ResourceType")
-//                        override fun afterHookedMethod(param: MethodHookParam?) {
-//                            if (LicenseStatus.sDisableCommonHooks) return
-////                            if (!isEnabled) return
-//                            logi("TestForcedOriginal: PhotoPreviewLogicDefault invoked")
-//                            val ctx = param!!.thisObject as View
-//                            val sendOriginPhotoCheckbox: CheckBox = ctx.findViewById(ConfigTable.getConfig(ForcedSendOriginalPhoto::class.simpleName))
-////                           val sendOriginPhotoCheckbox: CheckBox = ctx.findViewById(2131375226)
-//                            sendOriginPhotoCheckbox.isChecked = true
-//                            Utils.logd("TestForcedOriginal: Return checkbox isChecked " + sendOriginPhotoCheckbox.isChecked)
-////                            Utils.logd("TestForcedOriginal: Set checkbox checked to send original photo")
-//                        }
-//                    })
-//                }
-//            }
+//            })
             true
         } catch (t: Throwable) {
             Utils.log(t)
