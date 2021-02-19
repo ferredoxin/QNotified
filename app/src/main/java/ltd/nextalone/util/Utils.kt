50c354beaef58f029e21d12023174081ad037696
NextAlone <12210746+NextAlone@users.noreply.github.com>
2021-02-19 11:41:27 +0800

    Update NA Utils
    Signed-off-by: NextAlone <12210746+NextAlone@users.noreply.github.com>

--- app/src/main/java/ltd/nextalone/util/Utils.kt
+++ app/src/main/java/ltd/nextalone/util/Utils.kt
@@ -1,3 +1,24 @@
+/*
+ * QNotified - An Xposed module for QQ/TIM
+ * Copyright (C) 2019-2021 dmca@ioctl.cc
+ * https://github.com/ferredoxin/QNotified
+ *
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version and our eula as published
+ * by ferredoxin.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ * <https://github.com/ferredoxin/QNotified/blob/master/LICENSE.md>.
+ */
 package ltd.nextalone.util
 
 import android.content.SharedPreferences
@@ -70,9 +91,15 @@ internal fun Member.replaceEmpty(baseHook: BaseDelayableHook) = this.replace(bas
     ""
 }
 
-internal fun Any?.get(objName: String, clz: Class<*>? = null): Any? {
-    return ReflexUtil.iget_object_or_null(this, objName, clz)
-}
+internal fun Any?.get(objName: String, clz: Class<*>? = null): Any? = ReflexUtil.iget_object_or_null(this, objName, clz)
+
+internal fun Any?.set(name: String, value: Any): Any = ReflexUtil.iput_object(this, name, value)
+
+internal fun Any?.set(name: String, clz: Class<*>?, value: Any): Any = ReflexUtil.iput_object(this, name, clz, value)
+
+internal fun Class<*>?.instance(vararg arg: Any?): Any = XposedHelpers.newInstance(this, *arg)
+
+internal fun Class<*>?.instance(type: Array<Class<*>>, vararg arg: Any?): Any = XposedHelpers.newInstance(this, type, *arg)
 
 internal fun Member.hook(callback: NAMethodHook) = try {
     XposedBridge.hookMethod(this, callback)
