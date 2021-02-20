eaa017b791774a91cda463143b9bda5b2a68eaf9
ketal178 <ketal@qq.com>
2021-02-20 20:32:18 +0800

    Fixed FakeBalance on TIM

--- app/src/main/java/me/ketal/hook/FakeBalance.kt
+++ app/src/main/java/me/ketal/hook/FakeBalance.kt
@@ -33,17 +33,21 @@ import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.core.widget.doAfterTextChanged
+import ltd.nextalone.util.clazz
 import ltd.nextalone.util.hookAfter
 import me.ketal.base.PluginDelayableHook
-import me.ketal.util.HookUtil.getField
+import me.ketal.util.HookUtil.findClass
 import me.ketal.util.HookUtil.getMethod
+import me.ketal.util.TIMVersion
 import me.singleneuron.qn_kernel.data.hostInfo
 import me.singleneuron.qn_kernel.data.requireMinQQVersion
+import me.singleneuron.qn_kernel.data.requireMinVersion
 import me.singleneuron.util.QQVersion
 import nil.nadph.qnotified.R
 import nil.nadph.qnotified.SyncUtils
 import nil.nadph.qnotified.config.ConfigManager
 import nil.nadph.qnotified.ui.CustomDialog
+import nil.nadph.qnotified.util.ReflexUtil
 import nil.nadph.qnotified.util.Toasts
 import nil.nadph.qnotified.util.Utils
 
@@ -67,7 +71,7 @@ object FakeBalance : PluginDelayableHook("ketal_qwallet_fakebalance") {
         }
     }
 
-    override fun isValid(): Boolean = requireMinQQVersion(QQVersion.QQ_8_0_0)
+    override fun isValid(): Boolean = requireMinVersion(QQVersion.QQ_8_0_0, TIMVersion.TIM_1_0_0)
 
     fun listener() = View.OnClickListener {
         try {
@@ -108,6 +112,7 @@ object FakeBalance : PluginDelayableHook("ketal_qwallet_fakebalance") {
                         this.init()
                     }
                 }
+                dialog.dismiss()
             }
         } catch (e: Exception) {
             Utils.log(e)
@@ -119,17 +124,21 @@ object FakeBalance : PluginDelayableHook("ketal_qwallet_fakebalance") {
             .getMethod(classLoader)
             ?.hookAfter(this) {
                 val ctx = it.thisObject as Activity
-                val id = ctx.resources.getIdentifier("root", "id", Utils.PACKAGE_NAME_QQ)
+                val id = ctx.resources.getIdentifier("root", "id", hostInfo.packageName)
                 val rootView = ctx.findViewById<ViewGroup>(id)
-                val headerView = "Lcom/qwallet/view/QWalletHeaderViewRootLayout;->a:Lcom/qwallet/view/QWalletHeaderView;"
-                    .getField(classLoader)
-                    ?.get(rootView)
-                val tv = "Lcom/qwallet/view/QWalletHeaderView;->a:Landroid/widget/TextView;"
-                    .getField(classLoader)
-                    ?.get(headerView) as TextView
-                tv.doAfterTextChanged {
-                    if (this.isEnabled && it.toString() != money)
-                        tv.text = money
+                val headerClass = "com.qwallet.view.QWalletHeaderView".findClass(classLoader)
+                val headerView = ReflexUtil.getFirstByType(rootView, headerClass)
+                val numAnimClass = "com.tencent.mobileqq.activity.qwallet.widget.NumAnim".clazz
+                for (f in headerClass.declaredFields) {
+                    if (f.type == numAnimClass) {
+                        f.isAccessible = true
+                        val numAnim = f.get(headerView)
+                        val tv = ReflexUtil.getFirstByType(numAnim, TextView::class.java)
+                        tv.doAfterTextChanged {
+                            if (this.isEnabled && it.toString() != money)
+                                tv.text = money
+                        }
+                    }
                 }
             }
         true
