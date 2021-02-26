67dc9d2b1000c67c87925a98e814d504d60d3163
Null Idle <xenonhydride@gmail.com>
2021-02-26 22:13:13 +0800

    Add future possible conflicting class name
    Signed-off-by: Null Idle <xenonhydride@gmail.com>

--- app/src/main/java/nil/nadph/qnotified/startup/HybridClassLoader.java
+++ app/src/main/java/nil/nadph/qnotified/startup/HybridClassLoader.java
@@ -84,6 +84,7 @@ public class HybridClassLoader extends ClassLoader {
     public static boolean isConflictingClass(String name) {
         return name.startsWith("androidx.") || name.startsWith("android.support.v4.")
             || name.startsWith("kotlin.") || name.startsWith("kotlinx.")
+            || name.startsWith("com.android.tools.r8.")
             || name.startsWith("com.google.android.material.") || name.startsWith("com.google.gson.")
             || name.startsWith("org.intellij.lang.annotations.") || name.startsWith("org.jetbrains.annotations.");
     }
