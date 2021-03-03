58585e02cdd78c5a16db77cc65d89941924f4ec0
NextAlone <12210746+NextAlone@users.noreply.github.com>
2021-03-03 13:06:18 +0800

    Add SimplifyBottomQzone
    Signed-off-by: NextAlone <12210746+NextAlone@users.noreply.github.com>

---
+++ app/src/main/java/ltd/nextalone/hook/SimplifyBottomQzone.kt
@@ -0,0 +1,49 @@
+/*
+ * QNotified - An Xposed module for QQ/TIM
+ * Copyright (C) 2019-2021 dmca@ioctl.cc
+ * https://github.com/ferredoxin/QNotified
+ *
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version and our eula as published
+ * by ferredoxin.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ * <https://github.com/ferredoxin/QNotified/blob/master/LICENSE.md>.
+ */
+package ltd.nextalone.hook
+
+import ltd.nextalone.util.hookBefore
+import ltd.nextalone.util.method
+import ltd.nextalone.util.replaceNull
+import me.singleneuron.qn_kernel.data.requireMinQQVersion
+import me.singleneuron.util.QQVersion
+import nil.nadph.qnotified.base.annotation.FunctionEntry
+import nil.nadph.qnotified.hook.CommonDelayableHook
+
+@FunctionEntry
+object SimplifyBottomQzone : CommonDelayableHook("na_simplify_bottom_bar_kt") {
+
+    @Throws(Exception::class)
+    override fun initOnce(): Boolean {
+        "com.tencent.mobileqq.activity.home.impl.TabFrameControllerImpl".method("addFrame")?.hookBefore(this) {
+            val clzName = (it.args[it.args.size - 2] as Class<*>).name
+            if (clzName == "com.tencent.mobileqq.leba.Leba") {
+                it.result = null
+            }
+        }
+        "com.tencent.mobileqq.activity.home.impl.TabFrameControllerImpl".method("updateRedTouch")?.replaceNull(this)
+        return true
+    }
+
+    override fun isValid() = requireMinQQVersion(QQVersion.QQ_8_5_5)
+
+}
