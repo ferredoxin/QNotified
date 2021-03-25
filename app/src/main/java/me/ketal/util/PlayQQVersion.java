edb354d2d81239f2b4d41754c36e194007c4e518
Ketal <ketal@qq.com>
2021-03-25 20:06:10 +0800

    Add PlayQQVersion

---
+++ app/src/main/java/me/ketal/util/PlayQQVersion.java
@@ -0,0 +1,30 @@
+/*
+ * QNotified - An Xposed module for QQ/TIM
+ * Copyright (C) 2019-2021 dmca@ioctl.cc
+ * https://github.com/ferredoxin/QNotified
+ *
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version and our eula as published
+ * by ferredoxin.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ * <https://github.com/ferredoxin/QNotified/blob/master/LICENSE.md>.
+ */
+package me.ketal.util;
+
+public class PlayQQVersion {
+
+    public static final long PlayQQ_8_2_9 = 1352;
+    public static final long PlayQQ_8_2_9_1 = 1353;
+    public static final long PlayQQ_8_2_10 = 1354;
+    public static final long PlayQQ_8_2_11 = 1380;
+}
