5ebdaf2aeba162d40892824a448451651f0e8e89
Ketal <ketal@qq.com>
2021-05-16 20:25:56 +0800

    Fix LayoutParams error

--- app/src/main/java/nil/nadph/qnotified/ui/InterceptLayout.java
+++ app/src/main/java/nil/nadph/qnotified/ui/InterceptLayout.java
@@ -21,8 +21,6 @@
  */
 package nil.nadph.qnotified.ui;
 
-import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
-
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
@@ -50,37 +48,12 @@ public class InterceptLayout extends LinearLayout {
 
     public static InterceptLayout setupRudely(View v) {
         ViewGroup parent = (ViewGroup) v.getParent();
-        int index = 0;
+        int index = parent.indexOfChild(v);
         ViewGroup.LayoutParams currlp = v.getLayoutParams();
-        for (int i = 0; i < parent.getChildCount(); i++) {
-            if (parent.getChildAt(i) == v) {
-                index = i;
-                break;
-            }
-        }
         parent.removeView(v);
         InterceptLayout layout = new InterceptLayout(v.getContext());
-        ViewGroup.LayoutParams lpOuter;
-        LinearLayout.LayoutParams lpInner;
-        if (currlp == null) {
-            lpOuter = new ViewGroup.LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
-            lpInner = new LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
-        } else if (currlp instanceof ViewGroup.MarginLayoutParams) {
-            lpOuter = currlp;
-            lpInner = new LayoutParams(currlp.width, currlp.height);
-            lpInner.bottomMargin = ((MarginLayoutParams) currlp).bottomMargin;
-            lpInner.topMargin = ((MarginLayoutParams) currlp).topMargin;
-            lpInner.leftMargin = ((MarginLayoutParams) currlp).leftMargin;
-            lpInner.rightMargin = ((MarginLayoutParams) currlp).rightMargin;
-            ((MarginLayoutParams) currlp).bottomMargin = ((MarginLayoutParams) currlp).topMargin
-                = ((MarginLayoutParams) currlp).leftMargin = ((MarginLayoutParams) currlp).rightMargin = 0;
-            lpOuter.height = lpOuter.width = WRAP_CONTENT;
-        } else {
-            lpOuter = new ViewGroup.LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
-            lpInner = new LinearLayout.LayoutParams(currlp.width, currlp.height);
-        }
-        layout.addView(v, lpInner);
-        parent.addView(layout, index, lpOuter);
+        layout.addView(v);
+        parent.addView(layout, index, currlp);
         return layout;
     }
 
