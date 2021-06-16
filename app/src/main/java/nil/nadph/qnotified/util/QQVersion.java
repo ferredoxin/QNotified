af8d3e69f887a5bb48604e38915add2f2132d240
NextAlone <12210746+NextAlone@users.noreply.github.com>
2021-06-16 08:14:36 +0800

    Add QQ 8.8.3
    Signed-off-by: NextAlone <12210746+NextAlone@users.noreply.github.com>

--- app/src/main/java/nil/nadph/qnotified/util/QQVersion.java
+++ app/src/main/java/nil/nadph/qnotified/util/QQVersion.java
@@ -58,4 +58,5 @@ public class QQVersion {
     public static final long QQ_8_7_5 = 1738;
     public static final long QQ_8_7_8 = 1758;
     public static final long QQ_8_8_0 = 1792;
+    public static final long QQ_8_8_3 = 1818;
 }
