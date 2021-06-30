f99584d50624c0857bd452d9c59fa0ce67ae30c8
gao_cai_sheng <qwq233@qwq2333.top>
2021-07-01 02:59:16 +0800

    Re-add user status check
    Signed-off-by: gao_cai_sheng <qwq233@qwq2333.top>

--- app/src/main/java/nil/nadph/qnotified/util/LicenseStatus.java
+++ app/src/main/java/nil/nadph/qnotified/util/LicenseStatus.java
@@ -21,25 +21,27 @@
  */
 package nil.nadph.qnotified.util;
 
-import java.io.IOException;
+import static nil.nadph.qnotified.util.Utils.log;
+import static nil.nadph.qnotified.util.Utils.loge;
+import static nil.nadph.qnotified.util.Utils.logi;
 
 import cc.ioctl.chiral.Molecule;
+import java.io.IOException;
 import me.singleneuron.qn_kernel.data.HostInfo;
 import nil.nadph.qnotified.BuildConfig;
 import nil.nadph.qnotified.activity.EulaActivity;
 import nil.nadph.qnotified.config.ConfigManager;
-
-import static nil.nadph.qnotified.util.Utils.log;
+import nil.nadph.qnotified.remote.TransactionHelper;
+import nil.nadph.qnotified.util.data.UserStatusConst;
 
 public class LicenseStatus {
 
     public static final String qn_eula_status = "qh_eula_status";//typo, ignore it
     public static final String qn_auth2_molecule = "qn_auth2_molecule";
     public static final String qn_auth2_chiral = "qn_auth2_chiral";
-    /**
-     * No longer true, but keep it here.
-     */
-    public static final boolean sDisableCommonHooks = false;
+
+    public static final boolean sDisableCommonHooks = LicenseStatus.isBlacklisted();
+    public static final String qn_user_auth_status = "qn_user_auth_status";
     private static Molecule mAuth2Mol = null;
     private static int[] mAuth2Chiral = null;
 
@@ -66,12 +68,66 @@ public class LicenseStatus {
         return getEulaStatus() == EulaActivity.CURRENT_EULA_VERSION;
     }
 
+
+    public static void setUserCurrentStatus() {
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                int currentStatus = TransactionHelper.getUserStatus(Utils.getLongAccountUin());
+                logi("User Current Status: "
+                    + ""+String.valueOf(currentStatus));
+                ConfigManager.getDefaultConfig().putInt(qn_user_auth_status, currentStatus);
+                try {
+                    ConfigManager.getDefaultConfig().save();
+                    logi("User Current Status in ConfigManager: "
+                        +ConfigManager.getDefaultConfig().getIntOrDefault(qn_user_auth_status,-1));
+                } catch (IOException e) {
+                    log(e);
+                    Toasts.error(HostInfo.getHostInfo().getApplication(), e.toString());
+                }
+            }
+        }).start();
+
+    }
+
     public static boolean isAsserted() {
         return BuildConfig.DEBUG;
     }
 
     public static boolean isInsider() {
-        return BuildConfig.DEBUG;
+        int currentStatus = ConfigManager.getDefaultConfig().getIntOrDefault(qn_user_auth_status, -1);
+        if(currentStatus == UserStatusConst.notExist){
+            LicenseStatus.setUserCurrentStatus();
+            currentStatus = ConfigManager.getDefaultConfig().getIntOrDefault(qn_user_auth_status, -1);
+        }
+        if (currentStatus == UserStatusConst.developer) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isBlacklisted() {
+        int currentStatus = ConfigManager.getDefaultConfig().getIntOrDefault(qn_user_auth_status, -1);
+        if(currentStatus == UserStatusConst.notExist){
+            LicenseStatus.setUserCurrentStatus();
+            currentStatus = ConfigManager.getDefaultConfig().getIntOrDefault(qn_user_auth_status, -1);
+        }
+        if (currentStatus == UserStatusConst.blacklisted) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isWhitelisted() {
+        int currentStatus = ConfigManager.getDefaultConfig().getIntOrDefault(qn_user_auth_status, -1);
+        if(currentStatus == UserStatusConst.notExist){
+            LicenseStatus.setUserCurrentStatus();
+            currentStatus = ConfigManager.getDefaultConfig().getIntOrDefault(qn_user_auth_status, -1);
+        }
+        if (currentStatus == UserStatusConst.whitelisted) {
+            return true;
+        }
+        return false;
     }
 
 }
