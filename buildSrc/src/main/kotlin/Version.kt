d2f637ed0d051e1a9056c50dea904d3a2080c48e
NextAlone <12210746+NextAlone@users.noreply.github.com>
2021-11-17 00:58:44 +0800

    Update kotlin and ksp version
    Signed-off-by: NextAlone <12210746+NextAlone@users.noreply.github.com>

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,11 +1,7 @@
 import org.gradle.api.JavaVersion
 
 object Version {
-
-    @JvmStatic
-    val kotlin = "1.5.31"
-
-    @JvmStatic
+    val kotlin = "1.6.0"
+    val ksp = "1.0.1"
     val java = JavaVersion.VERSION_11
-
 }
