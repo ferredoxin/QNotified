133b0792814ddf13bcb3b242cf1120bae9a7bc9f
keta1 <ketal@qq.com>
2021-12-21 19:47:25 +0800

    UI: Synchronize the night mode of qq

--- app/src/main/java/me/singleneuron/qn_kernel/ui/activity/SettingActivity.kt
+++ app/src/main/java/me/singleneuron/qn_kernel/ui/activity/SettingActivity.kt
@@ -1,9 +1,12 @@
 package me.singleneuron.qn_kernel.ui.activity
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import me.singleneuron.qn_kernel.tlb.UiTable
 import nil.nadph.qnotified.R
+import nil.nadph.qnotified.ui.ResUtils
+import nil.nadph.qnotified.util.Utils
 import org.ferredoxin.ferredoxinui.common.base.TitleAble
 import org.ferredoxin.ferredoxinui.qnotified_style.activity.MaiTungTMStyleActivity
 import org.ferredoxin.ferredoxinui.qnotified_style.fragment.MaiTungTMSettingFragment
@@ -18,6 +21,12 @@ class SettingActivity<T> : MaiTungTMStyleActivity<T>() where T : Fragment, T : T
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_MaiTungTMUI)
+        runCatching {
+            AppCompatDelegate.setDefaultNightMode(
+                if (ResUtils.isInNightMode()) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO)
+        }.onFailure {
+            Utils.log(it)
+        }
         super.onCreate(savedInstanceState)
     }
 }
