93778c1bb286d3729051d48a12b34339761f1ac2
qwq233 <qwq233@qwq2333.top>
2022-01-22 16:37:57 +0800

    feat: hide Settings for blacklisted user
    Signed-off-by: qwq233 <qwq233@qwq2333.top>

--- app/src/main/java/me/singleneuron/qn_kernel/tlb/UiTable.kt
+++ app/src/main/java/me/singleneuron/qn_kernel/tlb/UiTable.kt
@@ -24,6 +24,7 @@ package me.singleneuron.qn_kernel.tlb
 
 import me.singleneuron.qn_kernel.ui.fragment.*
 import nil.nadph.qnotified.activity.TroubleshootActivity
+import nil.nadph.qnotified.util.LicenseStatus
 import org.ferredoxin.ferredoxinui.common.base.*
 
 val UiTable = uiScreen {
@@ -33,45 +34,58 @@ val UiTable = uiScreen {
         ActivityRouter.title to ActivityRouter,
         uiCategory {
             name = "模块设置"
-            contains = linkedMapOf(
-                uiClickableItem {
-                    title = "QQ净化"
-                    onClickListener = ClickToActivity(me.zpp0196.qqpurify.activity.MainActivity::class.java)
-                },
-                uiClickableItem {
-                    title = "花Q"
-                    onClickListener = ClickToNewSetting(RikkaQ)
-                },
-                uiClickableItem {
-                    title = "净化功能"
-                    onClickListener = ClickToNewPages(Purify)
-                },
-                uiClickableItem {
-                    title = "增强功能"
-                    onClickListener = ClickToNewSetting(Enhance)
-                },
-                uiClickableItem {
-                    title = "辅助功能"
-                    onClickListener = ClickToNewSetting(Auxiliary)
-                },
-                uiClickableItem {
-                    title = "其他功能"
-                    onClickListener = ClickToNewSetting(Others)
-                }
-            )
+            contains = if (!LicenseStatus.isBlacklisted()) {
+                linkedMapOf(
+                    uiClickableItem {
+                        title = "QQ净化"
+                        onClickListener = ClickToActivity(me.zpp0196.qqpurify.activity.MainActivity::class.java)
+                    },
+                    uiClickableItem {
+                        title = "花Q"
+                        onClickListener = ClickToNewSetting(RikkaQ)
+                    },
+                    uiClickableItem {
+                        title = "净化功能"
+                        onClickListener = ClickToNewPages(Purify)
+                    },
+                    uiClickableItem {
+                        title = "增强功能"
+                        onClickListener = ClickToNewSetting(Enhance)
+                    },
+                    uiClickableItem {
+                        title = "辅助功能"
+                        onClickListener = ClickToNewSetting(Auxiliary)
+                    },
+                    uiClickableItem {
+                        title = "其他功能"
+                        onClickListener = ClickToNewSetting(Others)
+                    }
+                )
+            } else {
+                linkedMapOf()
+            }
         },
         uiCategory {
             name = "其他设置"
-            contains = linkedMapOf(
-                uiClickableItem {
-                    title = "参数设定"
-                    onClickListener = ClickToNewSetting(Config)
-                },
-                uiClickableItem {
-                    title = "故障排查"
-                    onClickListener = ClickToActivity(TroubleshootActivity::class.java)
-                }
-            )
+            contains = if (!LicenseStatus.isBlacklisted()) {
+                linkedMapOf(
+                    uiClickableItem {
+                        title = "参数设定"
+                        onClickListener = ClickToNewSetting(Config)
+                    },
+                    uiClickableItem {
+                        title = "故障排查"
+                        onClickListener = ClickToActivity(TroubleshootActivity::class.java)
+                    }
+                )
+            } else {
+                linkedMapOf(
+                    uiClickableItem {
+                        title = "故障排查"
+                        onClickListener = ClickToActivity(TroubleshootActivity::class.java)
+                    }
+                )
+            }
         },
         uiCategory {
             name = "更多"
@@ -88,3 +102,4 @@ val UiTable = uiScreen {
         }
     )
 }
+
